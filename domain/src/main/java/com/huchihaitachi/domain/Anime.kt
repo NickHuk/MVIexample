70969dbe3f6bc271cbee88c9312a310939a661d4
NickHuk <mykola.huk@uptech.team>
2021-06-11 18:32:21 +0300

    implement equals/hashCode, method for instance copying

--- domain/src/main/java/com/huchihaitachi/domain/Anime.kt
+++ domain/src/main/java/com/huchihaitachi/domain/Anime.kt
@@ -13,4 +13,63 @@ open class Anime(
   val bannerImage: String? = null,
   override val timeOfBirth: Long = 0,
   override val timeToStale: Long = 0
-) : Dirtyable
+) : Dirtyable {
+
+  override fun equals(other: Any?) =
+    this === other || (other is  Anime)
+      && id == other.id
+      && title == other.title
+      && type == other.type
+      && description == other.description
+      && season == other.season
+      && seasonYear == other.seasonYear
+      && episodes == other.episodes
+      && duration == other.duration
+      && coverImage == other.coverImage
+      && bannerImage == other.bannerImage
+      && timeOfBirth == other.timeOfBirth
+      && timeToStale == other.timeToStale
+
+  override fun hashCode(): Int {
+    return id * 31 +
+      (title?.hashCode() ?: 0) * 31 +
+      (type?.hashCode() ?: 0) * 31 +
+      (description?.hashCode() ?: 0) * 31 +
+      (season?.hashCode() ?: 0) * 31 +
+      (seasonYear ?: 0) * 31 +
+      (episodes ?: 0) * 31 +
+      (duration ?: 0) * 31 +
+      (coverImage?.hashCode() ?: 0) * 31 +
+      (bannerImage?.hashCode() ?: 0) * 31 +
+      timeOfBirth.hashCode() * 31 +
+      timeToStale.hashCode() * 31
+  }
+
+  fun copy(
+    id: Int = this.id,
+    title: String? = this.title,
+    type: Type? = this.type,
+    description: String? = this.description,
+    season: Season? = this.season,
+    seasonYear: Int? = this.seasonYear,
+    episodes: Int? = this.episodes,
+    duration: Int? = this.duration, //minutes in general
+    coverImage: String? = this.coverImage,
+    bannerImage: String? = this.bannerImage,
+    timeOfBirth: Long = this.timeOfBirth,
+    timeToStale: Long = this.timeToStale
+  ) = Anime(
+    id,
+    title,
+    type,
+    description,
+    season,
+    seasonYear,
+    episodes,
+    duration,
+    coverImage,
+    bannerImage,
+    timeOfBirth,
+    timeToStale
+  )
+}
