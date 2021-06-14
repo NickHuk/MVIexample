c33ec14346d4ec319b4a32a576973c23888c71d5
NickHuk <mykola.huk@uptech.team>
2021-06-14 19:20:46 +0300

    emit initial state

--- base/src/main/java/com/huchihaitachi/base/BasePresenter.kt
+++ base/src/main/java/com/huchihaitachi/base/BasePresenter.kt
@@ -7,14 +7,14 @@ abstract class BasePresenter<V : BaseView<S>, S : BaseViewState>(
   initialState: S,
   protected val rxSchedulers: RxSchedulers
 ) {
-  protected var state: S = initialState
+  private val observableState: BehaviorSubject<S> = BehaviorSubject.create()
+  protected var state: S = initialState.also(observableState::onNext)
     set(value) {
       field = value
       observableState.onNext(value)
     }
   protected val disposables: CompositeDisposable = CompositeDisposable()
   protected var view: V? = null
-  private val observableState: BehaviorSubject<S> = BehaviorSubject.create()
 
   fun bind(view: V) {
     this.view = view
